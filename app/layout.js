4cf0455e5df14eb53eaf204ae67fb6b74e4249b2
Enes Türkhan <165434346+enesturkhan@users.noreply.github.com>
2025-01-30 02:20:42 +0300

    new13

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,5 @@
-import { SpeedInsights } from "@vercel/speed-insights/next"
 import "./globals.css";
+import { SpeedInsights } from '@vercel/speed-insights/next';
 
 export default function RootLayout({ children }) {
   return (
--- next.config.js
+++ next.config.js
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: "standalone",
+    async rewrites() {
+        return {
+            beforeFiles: [
+                {
+                    source: '/api/:path*',
+                    destination: '/api/:path*'
+                }
+            ]
+        };
+    },
     async headers() {
         return [
             {
@@ -9,7 +19,7 @@ const nextConfig = {
                     { key: "Access-Control-Allow-Credentials", value: "true" },
                     { key: "Access-Control-Allow-Origin", value: "*" },
                     { key: "Access-Control-Allow-Methods", value: "GET,DELETE,PATCH,POST,PUT" },
-                    { key: "Access-Control-Allow-Headers", value: "Accept, Content-Type" },
+                    { key: "Access-Control-Allow-Headers", value: "Accept, Content-Type" }
                 ]
             }
         ];
